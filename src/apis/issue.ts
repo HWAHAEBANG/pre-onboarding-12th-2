a5e24641f5931c2ce874f5378ed3f77d897156c8
HHB <bcl0206@gmail.com>
2023-08-31 07:36:03 +0900

    style: 포매팅 적용

--- src/apis/issue.ts
+++ src/apis/issue.ts
@@ -2,12 +2,11 @@ import { AxiosPromise } from 'axios';
 import { instance } from '../configs/axios';
 import { Issue } from '../types/issueType';
 
-  const getIssue = (state: string, sort: string, page: number): AxiosPromise<Issue> => {
-    return instance({
-      url: `/repos/facebook/react/issues?state=${state}&sort=${sort}&per_page=20&page=${page}`,
-      method: 'get',
-    });
-  }
-
+const getIssue = (state: string, sort: string, page: number): AxiosPromise<Issue> => {
+  return instance({
+    url: `/repos/facebook/react/issues?state=${state}&sort=${sort}&per_page=15&page=${page}`,
+    method: 'get',
+  });
+};
 
 export default getIssue;
