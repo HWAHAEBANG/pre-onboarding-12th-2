34335ddae2e0efb8dac18a950828ca046ce6c6dd
HHB <bcl0206@gmail.com>
2023-08-30 18:50:47 +0900

    feat: detail 하위 컴포넌트 분리

--- src/pages/IssueDetailPage.tsx
+++ src/pages/IssueDetailPage.tsx
@@ -1,5 +1,32 @@
+import { useLocation } from 'react-router-dom';
+import { styled } from 'styled-components';
+import IssueDetail from '../components/issue-detail-page/IssueDetail';
+
 const IssueDetailPage = () => {
-  return <div>IssueDetailPage</div>;
+  const {
+    state: { issue },
+  } = useLocation();
+
+  return (
+    <IssueListContainer>
+      <IssueListInner>
+        <IssueDetail issue={issue} />
+      </IssueListInner>
+    </IssueListContainer>
+  );
 };
 
 export default IssueDetailPage;
+
+const IssueListContainer = styled.main`
+  /* background-color: yellow;// */
+  color: white;
+`;
+
+const IssueListInner = styled.section`
+  /* background-color: #18062f; // */
+  width: 1080px;
+  min-height: 100vh;
+  margin: 30px auto;
+  border-radius: 15px;
+`;
