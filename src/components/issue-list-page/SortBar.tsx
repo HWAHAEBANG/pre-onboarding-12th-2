726c51e8bf88b4d5570f751c17f8f445bff519da
HHB <bcl0206@gmail.com>
2023-08-30 17:32:04 +0900

    feat: issue상태 필터 버튼 ui 구현

--- src/components/issue-list-page/SortBar.tsx
+++ src/components/issue-list-page/SortBar.tsx
@@ -1,5 +1,43 @@
-const IssueItem = () => {
-  return <div>IssueItem</div>;
+import { FC } from 'react';
+import { styled } from 'styled-components';
+
+interface Props {
+  states: string[];
+}
+
+const SortBar: FC<Props> = ({ states }) => {
+  return (
+    <SortBarWrap>
+      {states.map((state, index) => (
+        <StyledButton key={index}>{state}</StyledButton>
+      ))}
+    </SortBarWrap>
+  );
 };
 
-export default IssueItem;
+export default SortBar;
+
+const SortBarWrap = styled.div`
+  display: flex;
+  padding: 0 20px;
+`;
+
+const StyledButton = styled.button`
+  background-color: #18062f; //
+  width: 150px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  /* border: .5px solid gold; */
+  border-radius: 10px 10px 0 0;
+  color: #a5a3a3;
+  cursor: pointer;
+
+  &:focus {
+    background-color: #411474; //
+    border-bottom: none;
+    color: white;
+  }
+`;
