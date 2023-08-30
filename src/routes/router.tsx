19305601e7979f3567e8d6e781b20545b1937404
HHB <bcl0206@gmail.com>
2023-08-30 10:19:39 +0900

    feat: router 구성 완료

---
+++ src/routes/router.tsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter } from "react-router-dom";
+import IssueListPage from "../pages/IssueListPage";
+import NotFoundPage from "../pages/NotFoundPage";
+import App from "../App";
+import IssueDetailPage from "../pages/IssueDetailPage";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App/>,
+        errorElement: <NotFoundPage/>,
+        children: [
+            {
+                index: true,
+                element: <IssueListPage/>
+            },
+            {
+                path: '/detail/:id',
+                element: <IssueDetailPage/>
+            }
+        ]
+    }
+])
+
+export default router;
