2a2b3a35d4895be38ba720ec39b1a43337cffa80
HHB <bcl0206@gmail.com>
2023-08-30 17:31:30 +0900

    feat: lssue-list-section 컴포넌트 추가

---
+++ src/components/issue-list-page/IssueListSection.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react';
+import { styled } from 'styled-components';
+import IssueItem from './IssueItem';
+
+interface Props {
+  issueList: any[]; //타입 수정 요망
+}
+
+const IssueListSection: FC<Props> = ({ issueList }) => {
+  return (
+    <IssueListSectionWrap>
+      {issueList.map((issue, index) => (
+        <IssueItem issue={issue} key={index} />
+      ))}
+    </IssueListSectionWrap>
+  );
+};
+
+export default IssueListSection;
+
+const IssueListSectionWrap = styled.section`
+  /* background-color: #390c74; // */
+  border-radius: 15px;
+  box-shadow: 5px 5px 15px 5px rgba(0, 0, 0, 0.64);
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  gap: 1px;
+`;
