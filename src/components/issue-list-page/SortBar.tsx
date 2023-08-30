03f2fbfd0e4b8b3b2aa7c0d47d6b6d89fc803d5d
HHB <bcl0206@gmail.com>
2023-08-31 07:36:39 +0900

    feat: 필터 기능 구현

--- src/components/issue-list-page/SortBar.tsx
+++ src/components/issue-list-page/SortBar.tsx
@@ -1,16 +1,47 @@
-import { FC } from 'react';
+import { FC, MouseEvent, Dispatch, SetStateAction, ChangeEvent } from 'react';
 import { styled } from 'styled-components';
+
+type Filter = { state: string; sort: string; page: number };
 
 interface Props {
   states: string[];
+  sorts: string[];
+  searchFilter: Filter;
+  setSearchFilter: Dispatch<SetStateAction<Filter>>;
 }
 
-const SortBar: FC<Props> = ({ states }) => {
+const SortBar: FC<Props> = ({ states, sorts, searchFilter, setSearchFilter }) => {
+  const stateFilterClick = (e: MouseEvent<HTMLButtonElement>) => {
+    const buttonElement = e.target as HTMLButtonElement;
+    const buttonText = buttonElement.innerText;
+    setSearchFilter((prevState) => ({ ...prevState, state: buttonText }));
+  };
+
+  const sortFilterClick = (e: ChangeEvent<HTMLSelectElement>) => {
+    const selectedValue = e.target.value;
+    setSearchFilter((prevState) => ({ ...prevState, sort: selectedValue }));
+  };
+
   return (
     <SortBarWrap>
-      {states.map((state, index) => (
-        <StyledButton key={index}>{state}</StyledButton>
-      ))}
+      <StatesWrap>
+        {states.map((state, index) => (
+          <StyledButton
+            className={state === searchFilter.state ? 'selected' : ''}
+            key={index}
+            onClick={stateFilterClick}
+          >
+            {state}
+          </StyledButton>
+        ))}
+      </StatesWrap>
+      <StyledSelect id="dropdown" value={searchFilter.sort} onChange={sortFilterClick}>
+        {sorts.map((sort, index) => (
+          <option key={index} value={sort}>
+            {sort}
+          </option>
+        ))}
+      </StyledSelect>
     </SortBarWrap>
   );
 };
@@ -19,7 +50,12 @@ export default SortBar;
 
 const SortBarWrap = styled.div`
   display: flex;
+  justify-content: space-between;
   padding: 0 20px;
+`;
+
+const StatesWrap = styled.div`
+  display: flex;
 `;
 
 const StyledButton = styled.button`
@@ -29,14 +65,22 @@ const StyledButton = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-
   border-radius: 10px 10px 0 0;
   color: #a5a3a3;
   cursor: pointer;
 
-  &:focus {
+  &.selected {
     background-color: #411474;
     border-bottom: none;
     color: white;
   }
+`;
+
+const StyledSelect = styled.select`
+  background-color: #18062f;
+  color: white;
+  border: none;
+  border-radius: 10px 10px 0 0;
+  width: 100px;
+  padding-left: 10px;
 `;
--- src/pages/IssueListPage.tsx
+++ src/pages/IssueListPage.tsx
@@ -3,25 +3,38 @@ import { styled } from 'styled-components';
 import SortBar from '../components/issue-list-page/SortBar';
 import IssueListSection from '../components/issue-list-page/IssueListSection';
 import getIssue from '../apis/issue';
+import { Filter } from '../types/filterType';
 
 const IssueListPage = () => {
   const states = ['all', 'open', 'closed']; // 성능
+  const sorts = ['created', 'updated', 'comments'];
   const [issueList, setIssueList] = useState<any>([]); // 타입 수정..
 
+  const [searchFilter, setSearchFilter] = useState<Filter>({
+    state: 'open',
+    sort: 'comments',
+    page: 1,
+  });
+
   useEffect(() => {
-    getIssue('open', 'comments', 1)
+    getIssue(searchFilter.state, searchFilter.sort, searchFilter.page)
       .then((response) => {
         setIssueList(response.data);
       })
       .catch((error) => {
         console.error(error);
       });
-  }, []);
+  }, [searchFilter.state, searchFilter.sort, searchFilter.page]);
 
   return (
     <IssueListContainer>
       <IssueListInner>
-        <SortBar states={states} />
+        <SortBar
+          states={states}
+          sorts={sorts}
+          searchFilter={searchFilter}
+          setSearchFilter={setSearchFilter}
+        />
         <IssueListSection issueList={issueList} />
       </IssueListInner>
     </IssueListContainer>
