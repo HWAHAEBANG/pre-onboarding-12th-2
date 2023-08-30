6055c09dee6ce7a04f9d8584a9b79223c2996700
HHB <bcl0206@gmail.com>
2023-08-30 20:42:35 +0900

    feat: getIssue로직 모듈화

--- src/pages/IssueListPage.tsx
+++ src/pages/IssueListPage.tsx
@@ -2,24 +2,15 @@ import { useState, useEffect } from 'react';
 import { styled } from 'styled-components';
 import SortBar from '../components/issue-list-page/SortBar';
 import IssueListSection from '../components/issue-list-page/IssueListSection';
-import axios from 'axios';
+import issue from '../apis/issue';
 
 const IssueListPage = () => {
   const states = ['all', 'open', 'closed']; // 성능
-  const [issueList, setIssueList] = useState([]);
-
-  const GITHUB_ACCESS_KEY = process.env.REACT_APP_GITHUB_ACCESS_KEY;
+  const [issueList, setIssueList] = useState<any>([]); // 타입 수정..
 
   useEffect(() => {
-    axios({
-      method: 'get',
-      url: 'https://api.github.com/repos/facebook/react/issues?state=open&sort=comments&per_page=30&page=1', 
-      headers: {
-        Accept: 'application/vnd.github+json',
-        Authorization: `Bearer ${GITHUB_ACCESS_KEY}`,
-        'X-GitHub-Api-Version': '2022-11-28',
-      },
-    })
+    issue
+      .getIssue('open', 'comments', 1)
       .then((response) => {
         setIssueList(response.data);
       })
