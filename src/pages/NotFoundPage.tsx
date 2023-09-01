463bef93dd0616faa81ae0ffeb54fd02a1c949c0
HHB <bcl0206@gmail.com>
2023-09-01 20:33:19 +0900

    feat: not-found 페이지 구현

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,29 @@
+import { styled } from 'styled-components';
+
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <IssueListContainer>
+      <IssueListInner>
+        <h2>⚠️ NotFound ⚠️</h2>
+        <p>페이지 주소를 다시 확인해주세요</p>
+      </IssueListInner>
+    </IssueListContainer>
+  );
 };
 
 export default NotFoundPage;
+
+const IssueListContainer = styled.main`
+  color: white;
+`;
+
+const IssueListInner = styled.section`
+  width: 1080px;
+  min-height: 100vh;
+  margin: 0 auto;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
