b8f608673ff83a8581b03ae6b1011644d3f89837
HHB <bcl0206@gmail.com>
2023-09-01 20:48:33 +0900

    fix: 배포 버전 액세스 키 제거

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -5,7 +5,7 @@ export const instance = axios.create({
   timeout: 4000,
   headers: {
     Accept: 'application/vnd.github+json',
-    Authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_KEY}`,
+    // Authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_KEY}`,
     'X-GitHub-Api-Version': '2022-11-28',
   },
 });
